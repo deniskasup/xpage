2c86edb8fbe944e194c441eaddfd63d1fb9b758b
Denis <48751239+deniskasup@users.noreply.github.com>
2020-08-17 01:03:42 +0500

    дошел до слайдера

---
+++ index.js
@@ -0,0 +1,47 @@
+// Styles
+import './src/sass/main.sass'
+
+// import Swiper styles
+import 'swiper/swiper-bundle.min.css';
+
+
+// fancybox
+import '@fancyapps/fancybox/dist/jquery.fancybox.min.css'
+import '@fancyapps/fancybox'
+
+
+// Swiper
+
+import Swiper, { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
+Swiper.use([Navigation, Pagination, Scrollbar, Autoplay]);
+
+
+// Burger
+document.querySelector('.hamburger').addEventListener('click', event => {
+    document.querySelector('.mobilemenu').classList.toggle('active')
+})
+
+
+$(() => {
+
+    $('[data-fancybox]').fancybox({
+        youtube: {
+            controls: 0,
+            showinfo: 0
+        }
+    })
+
+    const examplesSlider = new Swiper('.examples__slider', {
+        loop: true,
+        spaceBetween: '30',
+        navigation: {
+            nextEl: '.examples__slider-buttons .swiper-button-next',
+            prevEl: '.examples__slider-buttons .swiper-button-prev'
+        },
+        pagination: {
+            el: '.swiper-pagination'
+        }
+    })
+
+
+})
