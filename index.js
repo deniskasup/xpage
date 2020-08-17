bcd3a0bab3e3297c327c2ff909d6bc0d3b7dcb91
Denis <deniskasup@gmail.com>
2020-08-17 17:53:13 +0500

    футер и контакты

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ import '@fancyapps/fancybox'
 
 // Swiper
 
-import Swiper, { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
+import Swiper, {Navigation, Pagination, Scrollbar, Autoplay} from 'swiper';
+
 Swiper.use([Navigation, Pagination, Scrollbar, Autoplay]);
 
 
@@ -21,6 +22,18 @@ document.querySelector('.hamburger').addEventListener('click', event => {
     document.querySelector('.mobilemenu').classList.toggle('active')
 })
 
+const examplesSlider = new Swiper('.examples__slider .swiper-container', {
+    loop: true,
+    slidesPerView: 'auto',
+    // spaceBetween: '30',
+    navigation: {
+        nextEl: '.examples__slider-buttons .swiper-button-next',
+        prevEl: '.examples__slider-buttons .swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    }
+})
 
 $(() => {
 
@@ -28,18 +41,6 @@ $(() => {
         youtube: {
             controls: 0,
             showinfo: 0
-        }
-    })
-
-    const examplesSlider = new Swiper('.examples__slider', {
-        loop: true,
-        spaceBetween: '30',
-        navigation: {
-            nextEl: '.examples__slider-buttons .swiper-button-next',
-            prevEl: '.examples__slider-buttons .swiper-button-prev'
-        },
-        pagination: {
-            el: '.swiper-pagination'
         }
     })
 
